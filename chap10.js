d911d3fc763b2901f82e7120c3993469b229e06a
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-09-26 20:02:29 +0100

    completed coding time!

--- chap10.js
+++ chap10.js
@@ -81,13 +81,13 @@ const governmentForms = [
 
 // TODO: compute the formsEndingWithCy array
 const names = forms => forms.map(form => form.name)
-const filter = (forms, func) => forms.filter(func)
+// const filter = (forms, func) => forms.filter(func)
 const endsWithCy = (form) => form.name.endsWith("cy")
 
-const formsEndingWithCy = names(filter(governmentForms, endsWithCy))
+// const formsEndingWithCy = names(filter(governmentForms, endsWithCy))
 
 // Should show ["Plutocracy", "Kleptocracy", "Theocracy", "Democracy", "Autocracy"]
-console.log(formsEndingWithCy);
+// console.log(formsEndingWithCy);
 
 const arrays = [[1, 4], [11], [3, 5, 7]];
 
@@ -95,6 +95,59 @@ const arrays = [[1, 4], [11], [3, 5, 7]];
 const arrSum = (arrs) => arrs.reduce((acc, curr) => acc + curr, 0)
 const arr2 = arr => arr.map(array => arrSum(array))
 
-const arraysSum = arrSum(arr2(arrays))
+// const arraysSum = arrSum(arr2(arrays))
 
-console.log(arraysSum); // Should show 31
+// console.log(arraysSum); // Should show 31
+
+//  Student result
+const students = [
+    {
+        name: "Anna",
+        sex: "f",
+        grades: [4.5, 3.5, 4]
+    },
+    {
+        name: "Dennis",
+        sex: "m",
+        country: "Germany",
+        grades: [5, 1.5, 4]
+    },
+    {
+        name: "Martha",
+        sex: "f",
+        grades: [5, 4, 2.5, 3]
+    },
+    {
+        name: "Brock",
+        sex: "m",
+        grades: [4, 3, 2]
+    }
+];
+
+// Compute female student results
+// const femaleStudentsResults = [];
+// for (const student of students) {
+//     if (student.sex === "f") {
+//         let gradesSum = 0;
+//         for (const grade of student.grades) {
+//             gradesSum += grade;
+//         }
+//         const averageGrade = gradesSum / student.grades.length;
+//         femaleStudentsResults.push({
+//             name: student.name,
+//             avgGrade: averageGrade
+//         });
+//     }
+// }
+
+// To-Do :
+const femaleOnly = el => el.sex === "f"
+const filter = (arr, func) => arr.filter(func)
+const averageGrade = arr => (arr.grades.reduce((acc, el) => acc + el, 0)) / arr.grades.length
+const femaleStudents = filter(students, femaleOnly)
+
+const femaleStudentsResults = femaleStudents.map(el => {
+    return { name: el.name, avgGrade: averageGrade(el) }
+})
+
+console.log(femaleStudentsResults);
